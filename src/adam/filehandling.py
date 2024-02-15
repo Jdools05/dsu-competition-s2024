2119c1e711b929e02b2e37702116542bc7eadeec
Adam Wood <woodadam272@gmail.com>
2024-02-14 21:42:23 -0700

    Adam Wood - added comments and better variable names to my code, and wrote 'useless.ipynb' to drop unneccesary labels

--- src/adam/filehandling.py
+++ src/adam/filehandling.py
@@ -3,36 +3,53 @@ import pandas as pd
 
 file_name = "../../data/DSU_output_FINAL.csv"
 
-def write_uniques(uniques):
-    max_wvals = 100
-    print("writing to unique")
-    with open("unique.txt", "w") as f:
-        f.write(f"Total of {len(uniques)} labels\n")
-        f.write(",".join(list(uniques)) + "\n\n")
-        useless = sum([ len(uniques[l]) <= 1 for l in uniques ] )
-        f.write(f"Number of useless labels: {useless}/{len(uniques)}\n\n")
-        for l in uniques:
-            u = uniques[l]
-            lnum = len(u)
-            f.write(f"Label: \"{l}\" length {lnum}\n")
-            for i in range( min( max_wvals, lnum ) ):
-                f.write(f"    {u[i]}\n")
-            if max_wvals < lnum:
-                f.write(f"     ... {lnum - max_wvals} more ... \n")
-            f.write("\n")
-    print("finished writing to unique")
-                    
-def get_uniques(uniques):
+# get unique values for each label
+# and write a sample of those values for each label to
+# a text file named unique.txt
+def get_uniques( df ):
+
+    unique_values = {}
+
     print(f"getting unique")
-    df = pd.read_csv( file_name )
+
+    # get unique values for each label
     labels = df.columns
-    for l in labels:
-        if not l in uniques:
-            uniques[l] = df[l].unique()
-                    
-def get_uniques_accross_all():
-    uniques = {}
-    get_uniques(uniques)
-    write_uniques( uniques )
+    for label in labels:
+        unique_values[label] = df[label].unique()
+
+    
+    max_write_vals = 100
+    label_amount = len(labels)
+    print("writing to unique")
+    
+    with open("unique.txt", "w") as fout:
+
+        # write number of labels along with the list
+        # of all labels
+        fout.write( f"Total of { label_amount } labels\n" )
+        fout.write( ",".join( labels ) + "\n\n" )
+
+        # compute the amount of labels with only one value in the entire column
+        # these are mostly all nans
+        useless = sum([ len( unique_values[ label ] ) <= 1 for label in labels ] )
+        fout.write( f"Number of useless labels: { useless }/{ label_amount }\n\n")
+        
+        for label in labels:
+            
+            total_values = unique_values[label]
+            total_amount = len( total_values )
+
+            # write the name of label and number of unique values in column
+            fout.write(f"Label: \"{ label }\" length { total_amount }\n")
+
+            #print a sample of those values up to max_write_vals
+            for i in range( min( max_write_vals, total_amount ) ):
+                fout.write(f"    { total_values[i] }\n")
+            if max_write_vals < total_amount:
+                fout.write(f"     ... {total_amount - max_write_vals} more ... \n")
+            fout.write("\n")
+    
+    print("finished writing to unique")
+                
             
             
